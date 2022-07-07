ac1cc88ae5a04bb553c339b05ea9883d0989c0ad
HP <HP@DESKTOP-QA7IFKP>
2022-07-07 16:35:54 +0530

    Build B01

---
+++ RestAssured/src/Manjunath/Multi.java
@@ -0,0 +1,15 @@
+package Manjunath;
+
+import java.util.List;
+
+public class Multi
+{
+  private List<Data> data;
+  private Support Support;
+public List<Data> getData() {
+	return data;
+}
+public Support getSupport() {
+	return Support;
+}
+}
